5d62d64d262ace03156239d70e63b3b6b6c809fc
Natalie Wyll <nataliewyll@yahoo.com>
2017-11-08 20:47:39 +0000

    removed spec from charges_controller_spec

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ChargesController, type: :controller do
   let(:user) { create(:user) }
-  let(:private_wiki) { create(:wiki, private: true, user: user) }
   
   before do
     sign_in(user)
@@ -12,25 +11,6 @@ RSpec.describe ChargesController, type: :controller do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
-    end
-  end
-  
-  describe "POST #create" do
-    it "changes user role to premium" do
-      post :create
-      expect(user.role).to eq("premium")
-    end
-  end
-  
-  describe "POST #downgrade" do
-    it "changes user role to standard" do
-      post :downgrade
-      expect(user.role).to eq("premium")
-    end
-    
-    it "changes all user wikis to public" do
-      post :downgrade
-      expect(private_wiki.private).to be false
     end
   end
 end
